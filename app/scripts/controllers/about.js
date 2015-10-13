a2d74bf9335163cd6ed303d3ea8dc5035bbdd253
Kiuby88 <kiuby88.programming@gmail.com>
2015-10-14 00:09:33 +0200

    Adding first files. Landing page.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rocketApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rocketApp
+ */
+angular.module('rocketApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rocketApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rocketApp
+ */
+angular.module('rocketApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
